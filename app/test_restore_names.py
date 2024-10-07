6b2de6207d580906b873ef1d00cfd0c96496ab9d
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2024-10-07 22:03:08 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,41 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "users,expected_users",
+    [
+        pytest.param(
+            [{
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }],
+            [{
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            }],
+            id=("Test when 'first_name' is None, "
+                "it should restore from 'full_name'"
+                )
+        ),
+        pytest.param(
+            [{
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            }],
+            [{
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            }],
+            id=("Test when 'first_name' doesn't exist, "
+                "it should restore from 'full_name'"
+                )
+        )
+    ]
+)
+def test_should_(users: [dict], expected_users: [dict]) -> None:
+    restore_names(users)
+    assert users == expected_users
